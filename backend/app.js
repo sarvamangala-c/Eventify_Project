403dab27ba0aed3e2c39a10138eb330b4d3491d6
sarvamangala-c <sarvamangala123456789@gmail.com>
2026-07-01 21:54:40 +0530

    Add custom domain aliases - eventify-mc.vercel.app and eventify-api.vercel.app

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL_NEW || process.env.FRONTEND_URL],
     methods: ["GET", "POST"],
     credentials: true,
   })
