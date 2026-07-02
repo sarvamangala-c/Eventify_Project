e7b8037139b4485468dbddf450310715119d99b4
sarvamangala-c <sarvamangala123456789@gmail.com>
2026-07-02 23:33:57 +0530

    Fix database timeout issues - increase connection timeouts and add reconnection logic

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -4,8 +4,9 @@ export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, { 
       dbName: "EventOrganize",
-      serverSelectionTimeoutMS: 5000,
+      serverSelectionTimeoutMS: 30000,
       socketTimeoutMS: 45000,
+      connectTimeoutMS: 30000,
     })
     .then(() => {
       console.log("Connected to database!");
